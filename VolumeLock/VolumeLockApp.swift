fffb7ca5462abdbbb84ec681e2a2387572d0024c
Shahzaib Ali <alishahzaib02@gmail.com>
2025-11-22 23:47:13 +0500

    Initial Commit

---
+++ VolumeLock/VolumeLockApp.swift
@@ -0,0 +1,17 @@
+//
+//  VolumeLockApp.swift
+//  VolumeLock
+//
+//  Created by Shahzaib Ali on 22/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct VolumeLockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
